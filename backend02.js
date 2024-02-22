4b684bf3e092baab85f63b2bb21292d964f45bf2
BIMAKHANSA <157813476+BIMAKHANSA@users.noreply.github.com>
2024-02-23 06:50:24 +0700

    Update backend02.js

--- backend02.js
+++ backend02.js
@@ -39,13 +39,15 @@ let dbStockObat = [
 app.use(bodyParser.json());
 
 // Endpoint untuk menambahkan user baru
-app.post('/user', (req, res) => {
+app.post('/user', function(req, res) {
     const { username, fullName } = req.body;
     if (!username || !fullName) {
         return res.status(400).json({ message: 'Username dan fullName harus diisi' });
     }
 
-    const existingUser = dbUser.find(user => user.username === username);
+    const existingUser = dbUser.find(function(user) {
+        return user.username === username;
+    });
     if (existingUser) {
         return res.status(409).json({ message: 'Username tersebut sudah tersedia, mohon diganti' });
     }
@@ -56,32 +58,37 @@ app.post('/user', (req, res) => {
         itemBuy: []
     });
 
-    res.status(201).json({ message: `User ${username} dengan Nama Lengkap ${fullName} sudah berhasil dibuat. Selamat belanja!` });
+    res.status(201).json({ message: 'User ' + username + ' dengan Nama Lengkap ' + fullName + ' sudah berhasil dibuat. Selamat belanja!' });
 });
 
 // Endpoint untuk membaca stok obat
-app.get('/stock', (req, res) => {
-    res.json(dbStockObat.reduce((acc, [item, stock]) => {
-        acc[item] = stock;
-        return acc;
-    }, {}));
+app.get('/stock', function(req, res) {
+    let stock = {};
+    dbStockObat.forEach(function(item) {
+        stock[item[0]] = item[1];
+    });
+    res.json(stock);
 });
 
 // Endpoint untuk membaca semua pengguna
-app.get('/users', (req, res) => {
+app.get('/users', function(req, res) {
     res.json(dbUser);
 });
 
 // Endpoint untuk menghapus riwayat pembelian dengan stok tertentu
-app.delete('/purchase-history/:itemName', (req, res) => {
+app.delete('/purchase-history/:itemName', function(req, res) {
     const itemName = req.params.itemName;
-    dbUser.forEach(user => {
-        user.itemBuy = user.itemBuy.filter(([item]) => item !== itemName);
+    dbUser.forEach(function(user) {
+        user.itemBuy = user.itemBuy.filter(function(item) {
+            return item[0] !== itemName;
+        });
     });
     res.status(204).send();
 });
 
 // Menjalankan server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, function() {
+    console.log('Server is running on http://localhost:' + PORT);
 });
+
+// Endpoint untuk menambahkan user baru
